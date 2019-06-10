5cb7eda9fd78b532adbbb4f70990a7ee00ca29a1
RJ Salamanca <salamancaronito@gmail.com>
2019-06-10 12:57:23 -0400

    refactored login and sign up posts moved bcrypt to our controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,10 @@
-const Users = require('../models/users');
+const Users = require('../models/users'),
+    bcrypt = require('bcryptjs');
+
+
+///////////////
+// PAGE GETS //
+///////////////
 
 exports.user_page_get = async (req, res) => {
     const userInstance = new Users(req.session.user_id,null, null,null,null);
@@ -40,4 +46,52 @@ exports.sign_up_get = (req,res) => {
             partial: 'partial-signup-form'
         }
     });
+}
+
+exports.logout = (req,res) =>{
+    console.log('logging out');
+    req.session.destroy();
+    res.redirect('/');
+}
+
+////////////////
+// PAGE POSTS //
+////////////////
+
+exports.login_page_post =  async (req,res) =>{
+    const { email, password } = req.body
+
+    const userInstance = new Users(null, null, null, email, password);
+
+    try{
+        let response = await userInstance.login();
+        req.session.is_logged_in = true;
+        req.session.first_name = response.first_name;
+        req.session.last_name = response.last_name;
+        req.session.user_id = response.id;
+
+        console.log('CORRECT PW!');
+        res.redirect('/users');
+    }catch(err){
+        console.log('WRONG PW!')
+        res.redirect('/users/signup');
+    }
+}
+
+exports.sign_up_post = async (req,res) =>{
+    const { first_name, last_name, email, password} = req.body
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt)
+    const userInstance = new Users(null, first_name, last_name, email, hash);
+
+    let check = await userInstance.checkIfCreated();
+
+    if(typeof check === 'object'){
+        res.redirect('/users/login');
+    } else {
+        await userInstance.save().then(response =>{
+            console.log('response is:', response);
+            res.redirect('/');
+        }).catch(err=>err)
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -1,58 +1,15 @@
 const express = require('express'),
     router = express.Router(),
-    Users = require('../models/users'),
-    userController = require('../controllers/users'),
-    bcrypt = require('bcryptjs');
-
+    userController = require('../controllers/users');
 
 // Gets
 router.get('/', userController.user_page_get);
 router.get('/login', userController.login_page_get);
 router.get('/signup', userController.sign_up_get);
+router.get('/logout', userController.logout);
 
 //Posts
-router.post('/login', async (req,res) =>{
-  const { email, password } = req.body
-
-  const userInstance = new Users(null, null, null, email, password);
-
-  try{
-      let response = await userInstance.login();
-      req.session.is_logged_in = true;
-      req.session.first_name = response.first_name;
-      req.session.last_name = response.last_name;
-      req.session.user_id = response.id;
-
-      console.log('CORRECT PW!');
-      res.redirect('/users');
-  }catch(err){
-      console.log('WRONG PW!')
-      res.redirect('/users/signup');
-  }
-});
-
-router.post('/signup', async (req,res) =>{
-  const { first_name, last_name, email, password} = req.body
-  const salt = bcrypt.genSaltSync(10);
-  const hash = bcrypt.hashSync(password, salt)
-  const userInstance = new Users(null, first_name, last_name, email, hash);
-
-  let check = await userInstance.checkIfCreated();
-
-  if(typeof check === 'object'){
-    res.redirect('/users/login');
-  } else {
-    await userInstance.save().then(response =>{
-        console.log('response is:', response);
-        res.redirect('/');
-    }).catch(err=>err)
-  }
-});
-
-router.get('/logout', (req,res) =>{
-  console.log('logging out');
-  req.session.destroy();
-  res.redirect('/');
-});
+router.post('/login', userController.login_page_post);
+router.post('/signup', userController.sign_up_post);
 
 module.exports = router;
