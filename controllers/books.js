3bccd9af71d6a8cec9c4df199d511eefda5ad354
RJ Salamanca <salamancaronito@gmail.com>
2019-06-10 13:06:52 -0400

    fully refactored books routes and controllers

--- controllers/books.js
+++ controllers/books.js
@@ -17,11 +17,32 @@ exports.books_page_get = async (req, res) => {
 }
 
 exports.single_book_page_get = async (req, res) => {
-    const { bookID } = req.params;
+    const { bookID } = req.params,
+        instanceBook = new Books(bookID, null, null),
+        getBook = await instanceBook.getOneBook(),
+        getBookReviews = await instanceBook.getOneBookReviews();
 
-    const instanceBook = new Books(bookID, null, null);
-    const getBook = await instanceBook.getOneBook();
-    const getBookReviews = await instanceBook.getOneBookReviews();
+    res.render('template', {
+        locals: {
+            title: 'Books Review Page',
+            book: getBook,
+            allBookReviews: getBookReviews,
+            is_logged_in: req.session.is_logged_in
+        },
+        partials: {
+            partial: 'partial-book-reviews'
+        }
+    });
+}
+
+exports.single_book_review_post = async function(req, res) {
+    const {review, score, id} = req.body,
+        instanceBook = new Books(id, null, null);
+    
+    await instanceBook.addReview(score, review, parseInt(req.session.user_id));
+    
+    const getBook = await instanceBook.getOneBook(),
+        getBookReviews = await instanceBook.getOneBookReviews();
 
     res.render('template', {
         locals: {
--- routes/books.js
+++ routes/books.js
@@ -1,33 +1,12 @@
 const express = require('express'),
     router = express.Router(),
-    Books = require('../models/books'),
     booksController = require('../controllers/books');
 
 // GETS
 router.get('/', booksController.books_page_get);
 router.get('/:bookID', booksController.single_book_page_get);
 
-router.post('/:bookID', async function(req, res) {
-    
-    const {review, score, id} = req.body;
-    const instanceBook = new Books(id, null, null);
-    
-    await instanceBook.addReview(score, review, parseInt(req.session.user_id));
-    
-    const getBook = await instanceBook.getOneBook();
-    const getBookReviews = await instanceBook.getOneBookReviews();
-
-    res.render('template', {
-        locals: {
-            title: 'Books Review Page',
-            book: getBook,
-            allBookReviews: getBookReviews,
-            is_logged_in: req.session.is_logged_in
-        },
-        partials: {
-            partial: 'partial-book-reviews'
-        }
-    });
-});
+// POSTS
+router.post('/:bookID', single_book_review_post);
 
 module.exports = router;
